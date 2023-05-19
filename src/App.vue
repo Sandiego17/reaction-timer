7de48e364e58bad2751debc6bd4016feeed8c93c
Henry Tochukwu Uzor <henry.uzor@cyphercrescent.com>
2023-05-19 16:46:00 +0100

    feat: custom events with data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <h1>Henry's Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResults">Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
-import { is } from '@babel/types'
 import Block from './components/Block.vue'
 
 export default {
@@ -14,14 +14,21 @@ export default {
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
-      // console.log(this.delay)
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
